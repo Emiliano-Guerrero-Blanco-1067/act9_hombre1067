2a904abfaad3cad2cae26bf43935e4f6f206354b
Emiliano Guerrero <164534739+Emiliano-Guerrero-Blanco-1067@users.noreply.github.com>
2024-09-20 11:51:55 -0600

    Add files via upload

---
+++ humano0777.py
@@ -0,0 +1,63 @@
+print("Act 8 clase humano")
+print("Emifish Guerrero 22308051281067")
+
+class Humano1967:
+    edad= 16
+    genero="hombre"
+    color_piel= "cafe"
+    telefono= "6567660246"
+    direccion="Portal Cesar"
+
+    grupo= "i"
+    semestre= "5to"
+    especialidad= "programacion"
+    NumControl= "1067"
+    #zona de creacion de objetos
+    def correr1067(self):
+        print(f"esta corriendo")
+
+    def saltar1067(self):
+        print(f"esta saltando")
+
+    def caminar1067(self):
+        print(f"esta caminando")
+
+    def trabajar1067(self):
+        print(f"esta trabajando")
+
+
+
+    def cocinar1067(self):
+        print(f"esta cocinando")
+
+    def bailar1067(self):
+        print(f"esta bailando")
+
+    def barrer1067(self):
+        print(f"esta barriendo")
+
+#zona de creacion de objetos
+emifish=Humano1967()
+maria=Humano1967()
+#zona deu usando objetos
+print("resultados para emifish")
+print(f"Edad: {emifish.edad}")
+print(f"genero: {emifish.genero}")
+print(f"Color de piel: {emifish.color_piel}")
+print(f"telefono: {emifish.telefono}")
+print(f"direccion: {emifish.direccion}")
+emifish.correr1067()
+emifish.saltar1067()
+emifish.trabajar1067()
+emifish.caminar1067()
+print(" ")
+print("-----------------------------------")
+print(" ")
+print("Resultado para Maria")
+print(f"Grupo: {maria.grupo}")
+print(f"Grupo: {maria.semestre}")
+print(f"Grupo: {maria.especialidad}")
+print(f"Grupo: {maria.NumControl}")
+maria.cocinar1067()
+maria.bailar1067()
+maria.barrer1067()
